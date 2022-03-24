206ff31620f7521aec1fbcd72d639b412e79b62a
Lidya1234 <lidya94gher@gmail.com>
2022-03-24 19:02:27 +0300

    Add components folder with three components

---
+++ src/components/AddTodo.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function AddTodo() {
+  return (
+    <div>     
+    <input type="text" placeholder="Enter todo" />
+    <button type="button">Add Todo</button>
+    </div>
+  )
+}
---
+++ src/components/Login.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Login() {
+  return (
+    <div>     
+    <input type="text" placeholder="Enter name" />
+    <button type="button">Login</button>
+    </div>
+  )
+}
