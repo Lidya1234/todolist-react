85fcd40aa63b629038122fa1ecab1d95660d6b71
Lidya1234 <lidya94gher@gmail.com>
2022-03-25 19:15:18 +0300

    Add src/components/TodoApp.jsx file to render add and list components

---
+++ src/components/TodoApp.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import AddTodo from './AddTodo'
+// import TodoList from './TodoList'
+
+export default function TodoApp() {
+  return (
+    <div>
+    <AddTodo /> </div>
+  )
+}
